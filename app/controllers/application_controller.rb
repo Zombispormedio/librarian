bae1864058318ca0ae7666fa9ab7fc0d19c9fb29
Xavier Serrano <zombispormedio007@gmail.com>
2016-12-31 21:14:51 +0100

    home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :set_locale
+
+  def set_locale
+    I18n.locale = extract_locale_from_header
+  end
+
+  private
+  def extract_locale_from_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  end
 end
